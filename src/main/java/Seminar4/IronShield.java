512f99b9352be12628ead26ec5ce47bb09db1671
Ruslan Karimov <ru-ka-@mail.ru>
2023-02-26 15:42:51 +0500

    added base: interface Shield, class WoodenShield and class IronShield

---
+++ src/main/java/Seminar4/IronShield.java
@@ -0,0 +1,26 @@
+package Seminar4;
+
+//На основе работы на уроке. Создать класс щита,
+// разработать разные типы щитов, добавить в семейство классов BaseHero обобщения в виде щитов.
+// Реструктуризировать код в конечных классах семейства Warriors.
+// Добавить метод определения минимального щита в команде.
+
+public class IronShield implements Shield{
+    private int defendPoint;
+
+    public IronShield(int defendPoint) {
+        this.defendPoint = defendPoint;
+    }
+
+    @Override
+    public int defend() {
+        return defendPoint;
+    }
+
+    @Override
+    public String toString() {
+        return "WoodenShield{" +
+                "defendPoint=" + defendPoint +
+                '}';
+    }
+}
